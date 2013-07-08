d29e90f332f853e7201e8f504728c9a90de52255
muupan <muupan@gmail.com>
2013-07-08 22:28:46 +0900

    Fix place of functor prefix of helper clauses.

--- src/sexpr_parser.cpp
+++ src/sexpr_parser.cpp
@@ -356,7 +356,7 @@ std::string ToProlog(const std::vector<TreeNode>& nodes, const bool quotes_atoms
   if (adds_helper_clauses) {
     const auto functors = CollectFunctorAtoms(nodes);
     for (const auto& functor_arity_pair : functors) {
-      const auto functor_atom = functor_prefix + (quotes_atoms ? '\'' + functor_arity_pair.first + '\'' : functor_arity_pair.first);
+      const auto functor_atom = quotes_atoms ? ('\'' + functor_prefix + functor_arity_pair.first + '\'') : (functor_prefix + functor_arity_pair.first);
       o << "user_defined_functor(" << functor_atom << ", " << functor_arity_pair.second << ")." << std::endl;
     }
   }
